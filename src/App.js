3a7cf25e760b8f2c06d9479eb11cb2812a0231ef
collins kiprotich <collinskiprotich@gmail.com>
2023-03-22 15:33:23 +0300

    Convert class components to functional components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './index.css';
-import { Component } from 'react';
 import Calculator from './components/Calculator';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Calculator />
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
 }
 
 export default App;
