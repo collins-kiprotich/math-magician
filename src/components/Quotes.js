f21283e54a04ac9a1d59ca4a04772392843be5af
collins kiprotich <collinskiprotich@gmail.com>
2023-03-24 16:43:30 +0300

    Fix linter error

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -2,36 +2,44 @@ import './Quotes.css';
 import { useEffect, useState } from 'react';
 
 function Quotes() {
+  const [quote, setQuote] = useState({});
 
-    const [quote, setQuote] = useState({});
+  const API_KEY = 'cnTlCQ/IFnLNbxvOCXF7fQ==9kiKfPTsk03LKemN';
+  useEffect(() => {
+    fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+      headers: { 'X-Api-Key': API_KEY },
+      method: 'GET',
+      contentType: 'application/json',
+    }).then((response) => response.json()).then((result) => {
+      if (result.error) {
+        setQuote({ quote: 'An Error Occurred' });
+      } else {
+        setQuote(result[0]);
+      }
+    });
+  }, []);
 
-    const API_KEY = "cnTlCQ/IFnLNbxvOCXF7fQ==9kiKfPTsk03LKemN";
-    useEffect(() => {
-        fetch("https://api.api-ninjas.com/v1/quotes?category=happiness", {
-            headers: { 'X-Api-Key': API_KEY },
-            method: 'GET',
-            contentType: 'application/json',
-        }).then((response) => response.json()).then((result) => {
-            if(result.error){
-                setQuote({ quote: "An Error Occurred" })
-            } else {
-                setQuote(result[0]);
-            }
-        });
-    }, []);
-
-    return (
-        !quote.quote ?
-            (<div className="loadingclip">
-                loading a quote....
+  return (
+  // eslint-disable-next-line no-nested-ternary
+    !quote.quote
+      ? (
+        <div className="loadingclip">
+          loading a quote....
+        </div>
+      )
+      : (quote.author
+        ? (
+          <div className="quotes">
+            <p>{quote.quote}</p>
+            <div>
+              &quot;
+              {quote.author}
+              &quot;
             </div>
-            )
-            :
-            (quote.author ?
-                (<div className="quotes"><p>{quote.quote}</p>
-                    <div>&quot;{quote.author}&quot;</div></div>) : (<div className="quotes"><p>{quote.quote}</p></div>)
-            )
-    )
+          </div>
+        ) : (<div className="quotes"><p>{quote.quote}</p></div>)
+      )
+  );
 }
 
 export default Quotes;
